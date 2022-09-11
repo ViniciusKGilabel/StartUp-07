5dd35a767555cc470ca537fc647aaf8a7648bdad
Bruna Cardoso de Lima <88549878+BrunaCLima@users.noreply.github.com>
2022-09-11 17:15:44 -0300

    trabalho Unifil

--- src/navigation/LoggedOutStack.js
+++ src/navigation/LoggedOutStack.js
@@ -10,7 +10,6 @@ function AppStack() {
       COMO FAZER O STACK NAVIGATION ABAIXO CADA SCREEN TEM A FUNÇÃO DE 1 PAGINA DO APP
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Notifications" component={Notifications} />
-      <Stack.Screen name="Profile" component={Profile} />
       <Stack.Screen name="Settings" component={Settings} /> */}
     </Stack.Navigator>
   );
